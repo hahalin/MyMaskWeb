ebba2f72c8f00d9c70dba1bcf5a6aba79dd7515f
FrankMacpro <hahalin@gmail.com>
2020-03-03 14:24:37 +0800

    fix poolid

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_HT7IzJab6 ap-southeast-1_HT7IzJab6', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'ap-southeast-1_HT7IzJab6', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7lfr601goluduvmgkntfuoqu1c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
